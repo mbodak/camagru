04e0fa8a056ee24c92a598658518797c188e55da
Maryna Bodak <bodak.marina.ev@gmail.com>
2017-07-20 18:51:00 +0300

    changes in footer

--- footer.php
+++ footer.php
@@ -8,12 +8,23 @@
 
 <body>
     <footer>
-        <div class="flex-footer">
-            <div class="footer-logo">
-
+        <div class="info">
+            <div class="head">
+                <p class="name">C a m a g r u</p>
             </div>
-
+            <div class="about">
+                <p>
+                    <a href="/" >About The Project</a>
+                    <br><br>
+                    <a href="/" >Contact Us</a>
+                    <br><br>
+                    <a href="/" >bodak.marina.ev@gmail.com</a>
+                    <br>
+                    <a href="/" >+38 (095) 447 48 12</a>
+                </p>
+            </div>
         </div>
+
 
     </footer>
 
