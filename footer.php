22c2fa82e909db61f6af77495399ab66ea6a3fe8
Marina <bodak.marina.ev@gmail.com>
2017-08-01 11:19:44 +0300

    slide-side panel

--- footer.php
+++ footer.php
@@ -14,18 +14,33 @@
             </div>
             <div class="about">
                 <p>
-                    <a href="/" >About The Project</a>
+                    <a href="">About The Project</a>
                     <br><br>
-                    <a href="/" >Contact Us</a>
+                    <a href="">Contact Us</a>
                     <br><br>
-                    <a href="/" >bodak.marina.ev@gmail.com</a>
+                    <a href="">bodak.marina.ev@gmail.com</a>
                     <br>
-                    <a href="/" >+38 (095) 447 48 12</a>
+                    <a href="">+38 (095) 447 48 12</a>
                 </p>
             </div>
+            <div class="social">
+                <div class="facebook">
+                    <a href="https://www.facebook.com/m.bodak.ev" target="_blank">
+                        <img class="link" src="">
+                    </a>
+                </div>
+                <div class="instagram">
+                    <a href="https://www.instagram.com/marynsbody/" target="_blank">
+                        <img class="link" src="">
+                    </a>
+                </div>
+                <div class="linkedin">
+                    <a href="https://www.linkedin.com/in/marina-bodak-a1a5abb4/" target="_blank">
+                        <img class="link" src="">
+                    </a>
+                </div>
+            </div>
         </div>
-
-
     </footer>
 
 </body>
