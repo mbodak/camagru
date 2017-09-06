47fd94f916f20bdae51c4086acb28e27ae0b9dba
Maryna Bodak <bodak.marina.ev@gmail.com>
2017-09-06 17:53:23 +0300

    +

--- take_photo.php
+++ take_photo.php
@@ -11,60 +11,102 @@
                     <ul>
                         <li>
                             <div>
-                                <a href="#"></a>
+                                <canvas id="canvas"></canvas>
                             </div>
                         </li>
                         <li>
                             <div>
-                                <a href="#"></a>
+                                <canvas id="canvas"></canvas>
                             </div>
                         </li>
                         <li>
                             <div>
-                                <a href="#"></a>
+                                <canvas id="canvas"></canvas>
                             </div>
                         </li>
                         <li>
                             <div>
-                                <a href="#"></a>
+                                <canvas id="canvas"></canvas>
                             </div>
                         </li>
                         <li>
                             <div>
-                                <a href="#"></a>
+                                <canvas id="canvas"></canvas>
                             </div>
                         </li>
                         <li>
                             <div>
-                                <a href="#"></a>
+                                <canvas id="canvas"></canvas>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <canvas id="canvas"></canvas>
                             </div>
                         </li>
                     </ul>
                 </div>
             </div>
 
-            <div class="for_photo">
-                <div class="camera">
-                    <div>
-                        <video id="video" poster="icons/logo.png" autoplay></video>
-                        <button id="snap">Snap Photo</button>
-                        <canvas id="canvas"></canvas>
-                    </div>
-                    <div>
-
-                    </div>
-
+            <div class="camera">
+                <div class="video">
+                    <video id="video" autoplay poster="/Camagru/icons/logo.png"></video>
+                    <button id="snap">Snap Photo</button>
                 </div>
+                <script>
+                    // Grab elements, create settings, etc.
+                    var video = document.getElementById('video');
+
+                    // Get access to the camera!
+                    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                        // Not adding `{ audio: true }` since we only want video now
+                        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+                            video.src = window.URL.createObjectURL(stream);
+                            video.play();
+                        });
+                    }
+                    // Elements for taking the snapshot
+
+                    var canvas = document.getElementById('canvas');
+                    var context = canvas.getContext('2d');
+                    var video = document.getElementById('video');
+
+                    // Trigger photo take
+                    document.getElementById("snap").addEventListener("click", function() {
+                        context.drawImage(video, 0, 0, 640, 480);
+                    });
+                </script>
                 <div class="tools">
+                    <div>
 
-                </div>
-                <div class="actions">
+                    </div>
+                    <div>
 
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
+
+            </div>
+
+            <div class="actions">
+
             </div>
         </div>
     </div>
 </main>
+
+
 
 
 <?php
