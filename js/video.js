3b0015dba8cb694bde8676c59cfa91cd7bc4a0a9
Maryna Bodak <bodak.marina.ev@gmail.com>
2017-09-14 19:19:13 +0300

    +

---
+++ js/video.js
@@ -0,0 +1,54 @@
+// // Grab elements, create settings, etc.
+// var video = document.getElementById('video');
+//
+// // Get access to the camera!
+// if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//     // Not adding `{ audio: true }` since we only want video now
+//     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+//         video.src = window.URL.createObjectURL(stream);
+//         video.play();
+//     });
+// }
+// // Elements for taking the snapshot
+//
+// var canvas = document.getElementById('canvas');
+// var context = canvas.getContext('2d');
+// video = document.getElementById('video');
+//
+// // Trigger photo take
+// document.getElementById("snap").addEventListener("click", function() {
+//     context.drawImage(video, 0, 0, 640, 480);
+// });
+
+
+//направляем поток из getUserMedia в video
+var video = document.querySelector("#video"),
+    canvas = document.querySelector('#canvas'),
+    ctx = canvas.getContext('2d'),
+    localMediaStream = null,
+    onCameraFail = function (e) {
+        console.log('Camera did not work.', e); // Исключение на случай, если камера не работает
+    };
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+navigator.getUserMedia({video: true}, function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    localMediaStream = stream;
+}, onCameraFail);
+
+
+//таймер, изображение постоянно копируется в canvas
+cameraInterval = setInterval(function() { snapshot();}, 1);
+function snapshot() {
+    if(localMediaStream){
+        canvas.width = document.getElementById('video').offsetWidth;
+        canvas.height = document.getElementById('video').offsetHeight;
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        // ctx.drawImage(video, 0, 0);
+    }
+}
+
+
+// canvas.width = document.getElementById('video').offsetWidth;
+// canvas.height = document.getElementById('video').offsetHeight;
+// ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
--- take_photo.php
+++ take_photo.php
@@ -2,6 +2,7 @@
     include ("head.php");
     include ("header.php");
 ?>
+<video id="video" autoplay poster="/Camagru/icons/logo.png"></video>
 <div class="forheader"></div>
 <main>
     <div class="page">
@@ -50,31 +51,12 @@
 
             <div class="camera">
                 <div class="video">
-                    <video id="video" autoplay poster="/Camagru/icons/logo.png"></video>
+
+                    <canvas id="canvas">
                 </div>
-                <script>
-                    // Grab elements, create settings, etc.
-                    var video = document.getElementById('video');
 
-                    // Get access to the camera!
-                    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                        // Not adding `{ audio: true }` since we only want video now
-                        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-                            video.src = window.URL.createObjectURL(stream);
-                            video.play();
-                        });
-                    }
-                    // Elements for taking the snapshot
 
-                    var canvas = document.getElementById('canvas');
-                    var context = canvas.getContext('2d');
-                    video = document.getElementById('video');
 
-                    // Trigger photo take
-                    document.getElementById("snap").addEventListener("click", function() {
-                        context.drawImage(video, 0, 0, 640, 480);
-                    });
-                </script>
                 <div class="tools">
                     <div title="select">
 
@@ -115,7 +97,7 @@
 
 
 
-
+<script type="text/javascript" src="/Camagru/js/video.js"></script>
 <?php
     include ("footer.php");
 ?>
