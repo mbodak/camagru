702949d87c0abc9ac40ff8573be89915cd0ae91b
Maryna Bodak <bodak.marina.ev@gmail.com>
2017-09-21 19:18:08 +0300

    icons done

--- js/video.js
+++ js/video.js
@@ -24,7 +24,7 @@
 //направляем поток из getUserMedia в video
 var video = document.querySelector("#video"),
     canvas = document.querySelector('#canvas'),
-    ctx = canvas.getContext('2d'),
+    context = canvas.getContext('2d'),
     localMediaStream = null,
     onCameraFail = function (e) {
         console.log('Camera did not work.', e); // Исключение на случай, если камера не работает
@@ -32,7 +32,10 @@ var video = document.querySelector("#video"),
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 window.URL = window.URL || window.webkitURL;
 navigator.getUserMedia({video: true}, function(stream) {
-    video.src = window.URL.createObjectURL(stream);
+    var obj_url = window.URL.createObjectURL(stream);
+    video.src = obj_url;
+    video.play();
+    window.URL.revokeObjectURL(obj_url);
     localMediaStream = stream;
 }, onCameraFail);
 
@@ -43,8 +46,20 @@ function snapshot() {
     if(localMediaStream){
         canvas.width = document.getElementById('video').offsetWidth;
         canvas.height = document.getElementById('video').offsetHeight;
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
     }
 }
+// document.getElementById("snap").addEventListener("click", function() {
+//      context.drawImage(video, 0, 0, 640, 480);
 
-//подстраиваем высоту блока tools по высоту video
+function imgOverlaying(src) {
+    var img = new Image();
+    img.onload = function () {
+        context.drawImage(img, 0, 0);
+    };
+    img.onerror = function () {
+        console.log('Broken image');
+    };
+    img.src = src;
+}
+
--- take_photo.php
+++ take_photo.php
@@ -2,11 +2,12 @@
     include ("head.php");
     include ("header.php");
 ?>
-<video id="video" autoplay poster="/Camagru/icons/logo.png"></video>
+
 <div class="forheader"></div>
-<main>
+<div class="main">
     <div class="page">
         <div class="wrapper">
+            <video id="video" autoplay poster="/Camagru/icons/logo.png"></video>
             <div class="gallery">
                 <div class="accordion">
                     <ul>
@@ -53,19 +54,42 @@
                 <div class="video">
                     <canvas id="canvas">
                 </div>
-
+<script>
+    function imgOverlaying(src) {
+        var img = new Image();
+        img.onload = function () {
+            context.drawImage(img, 0, 0);
+        };
+        img.onerror = function () {
+            console.log('Broken image');
+        };
+        img.src = src;
+    }
+</script>
                 <div class="tools">
-                    <div title="select"></div>
+                    <div title="select" onclick="imgOverlaying('/Camagru/stickers/face.png')">
+                        <img src="/Camagru/stickers/face.png">
+                    </div>
 
-                    <div></div>
+                    <div onclick="imgOverlaying('/Camagru/stickers/hair.png')">
+                        <img src="/Camagru/stickers/hair.png">
+                    </div>
 
-                    <div></div>
+                    <div onclick="imgOverlaying('/Camagru/stickers/deer.png')">
+                        <img src="/Camagru/stickers/deer.png">
+                    </div>
 
-                    <div></div>
+                    <div onclick="imgOverlaying('/Camagru/stickers/hat2.png')">
+                        <img src="/Camagru/stickers/hat2.png">
+                    </div>
 
-                    <div></div>
+                    <div onclick="imgOverlaying('/Camagru/stickers/mas.png')">
+                        <img src="/Camagru/stickers/mas.png">
+                    </div>
 
-                    <div></div>
+                    <div>
+                        <p>UPLOAD</p>
+                    </div>
 
                 </div>
 
@@ -84,9 +108,7 @@
             </div>
         </div>
     </div>
-</main>
-
-
+</div>
 
 <script type="text/javascript" src="/Camagru/js/video.js"></script>
 <?php
