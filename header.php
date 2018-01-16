03ca22a2eb9bbc93974403763ba15dac7d0e1fe1
mbodak <mbodak@student.unit.ua>
2018-01-16 20:40:24 +0200

    drop down menu added

--- header.php
+++ header.php
@@ -16,10 +16,18 @@
             </div>
 
             <div class="box-nav">
-                <button onclick="open_search()">
-<!--                    <i class="material-icons">search</i>-->
-                    <i class="material-icons">face</i>
-                </button>
+                <div class="dropdown">
+                    <button>
+                        <i class="material-icons">face</i>
+                    </button>
+
+                    <div class="dropdown-content">
+                        <a href="/Camagru/profile.php">Profile</a>
+                        <a href="/Camagru/modif.php">Change password</a>
+                        <a href="#">Notification ON</a>
+                        <a href="#">LOG OUT</a>
+                    </div>
+                </div>
             </div>
         </div>
         <aside class="w3-sidebar w3-bar-block w3-card-2 w3-animate-left" style="display:none" id="mySidebar">
@@ -43,20 +51,20 @@
                 </ul>
             </nav>
         </aside>
-        <div class="search-overlay" style="display: none;" id="mySearch">
-            <svg class="close-search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" onclick="close_search()">
-                <polygon points="64,5.9 58.1,0 32,26.1 5.9,0 0,5.9 26.1,32 0,58.1 5.9,64 32,37.9 58.1,64 64,58.1 37.9,32"></polygon>
-            </svg>
-            <form role="search" method="get" class="search-form" action="/Camagru/">
-                <div>
-                    <input type="text" value="" name="s" id="s" placeholder="Type and press enter">
-                </div>
-            </form>
-            <form role="search" method="get" class="search-form" action="/Camagru/">
-                <div>
-                    <input type="text" value="" name="s" id="s" placeholder="Type and press enter">
-                </div>
-            </form>
-        </div>
+<!--        <div class="search-overlay" style="display: none;" id="mySearch">-->
+<!--            <svg class="close-search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" onclick="close_search()">-->
+<!--                <polygon points="64,5.9 58.1,0 32,26.1 5.9,0 0,5.9 26.1,32 0,58.1 5.9,64 32,37.9 58.1,64 64,58.1 37.9,32"></polygon>-->
+<!--            </svg>-->
+<!--            <form role="search" method="get" class="search-form" action="/Camagru/">-->
+<!--                <div>-->
+<!--                    <input type="text" value="" name="s" id="s" placeholder="Type and press enter">-->
+<!--                </div>-->
+<!--            </form>-->
+<!--            <form role="search" method="get" class="search-form" action="/Camagru/">-->
+<!--                <div>-->
+<!--                    <input type="text" value="" name="s" id="s" placeholder="Type and press enter">-->
+<!--                </div>-->
+<!--            </form>-->
+<!--        </div>-->
     </header>
 </html>
