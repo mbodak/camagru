62c54716349018618325912ece5214e54ef2e758
mbodak <mbodak@student.unit.ua>
2018-10-26 15:06:46 +0300

    +

--- config/routes.php
+++ config/routes.php
@@ -9,7 +9,6 @@ return array (
     'camagru/logout' => 'account/logout',
     'camagru/modify' => 'account/modify',
     'camagru/forgot' => 'account/forgot',
-//    'camagru/activation/([a-zA-Z]+)/([a-zA-Z0-9]+)' => 'account/activation/$1/$2',
 
 );
 
