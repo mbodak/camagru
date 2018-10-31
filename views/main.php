43f89ff68666c23a01d34b3b59040da667ea83d1
mbodak <mbodak@student.unit.ua>
2018-10-31 22:41:52 +0200

    +

--- views/main.php
+++ views/main.php
@@ -16,13 +16,13 @@
                             <div class="photo-hover">
                                 <div class="set-like">
                                     '; if (LOGGED_IN) : print '
-                                    <i class="material-icons like" onclick="setLike('.$value["id"].', this.parentElement.parentElement)">thumb_up</i>
-                                    <i class="material-icons dislike" onclick="removeLike('.$value["id"].', this.parentElement.parentElement)">thumb_down</i>
+                                    <i class="material-icons like" style="color: red" onclick="setLike('.$value["id"].', this.parentElement.parentElement)">thumb_up</i>
+                                    <i class="material-icons dislike" style="color: red" onclick="removeLike('.$value["id"].', this.parentElement.parentElement)">thumb_down</i>
                                     '; endif; print '
                                 </div>
                                 <div class="count-likes">
                                     <i class="material-icons" style="color: red">favorite</i>
-                                    <i><b class="lco">'.$value["likes_count"].'</b></i>
+                                    <i><b class="lco" style="color: red">'.$value["likes_count"].'</b></i>
                                 </div>
                             </div>
                     </div>';
