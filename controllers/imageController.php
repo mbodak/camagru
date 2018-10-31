8673df995b65e4ad4b16f4a00e5b767ffa345e05
Alexander Stepanov <vaiol.ans@gmail.com>
2018-10-31 20:06:56 +0200

    45

--- controllers/imageController.php
+++ controllers/imageController.php
@@ -4,16 +4,42 @@ include_once ROOT.'/models/imageModel.php';
 class imageController
 {
     public function actionSave() {
+        if (!LOGGED_IN) {
+            exit();
+        }
         imageModel::save();
         return (true);
     }
     public function actionRemove() {
+        if (!LOGGED_IN) {
+            exit();
+        }
         if ($_POST && isset($_POST['id'])) {
             if (imageModel::remove(intval($_POST['id']))) {
                 header("Location: ".LinkService::getRoot()."profile#removed");
             } else {
                 header("Location: ".LinkService::getRoot()."profile#notRemoved");
             }
+        }
+        return (true);
+    }
+    public function actionLike() {
+        if (!LOGGED_IN) {
+            exit();
+        }
+        if ($_POST && isset($_POST['id'])) {
+            imageModel::like(intval($_POST['id']));
+        }
+        return (true);
+    }
+    public function actionDislike() {
+        if (!LOGGED_IN) {
+            exit();
+        }
+        if ($_POST && isset($_POST['id'])) {
+            if ($_POST && isset($_POST['id'])) {
+                imageModel::dislike(intval($_POST['id']));
+        }
         }
         return (true);
     }
