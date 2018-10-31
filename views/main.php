a96be6da9aab0c256b2b1696e706e453a5a49f1f
mbodak <bodak.marina.ev@gmail.com>
2018-10-31 20:29:08 +0200

    implement likes

--- views/main.php
+++ views/main.php
@@ -10,11 +10,19 @@
         $images = Images::getAll(0, 1000);
         foreach ($images as $value) {
             print '
-                    <div class="photo">
-                        <img src="images/'.$value["name"].'" alt="image">
-                        <div style="display: inline-block">'
-                            .$value["likes_count"].
-                        '</div>
+                    <div class="photo" onmouseover="showLikesCover(this)" onmouseleave="hideLikesCover(this)">
+                            <img src="images/'.$value["name"].'" alt="image">
+                            
+                            <div class="photo-hover">
+                                <div class="set-like">
+                                    <i class="material-icons like" onclick="setLike(this.parentElement.parentElement.parentElement)">thumb_up</i>
+                                    <i class="material-icons dislike" onclick="removeLike(this.parentElement.parentElement.parentElement)">thumb_down</i>
+                                </div>
+                                <div class="count-likes">
+                                    <i class="material-icons" style="color: red">favorite</i>
+                                    <i><b>'.$value["likes_count"].'</b></i>
+                                </div>
+                            </div>
                     </div>';
         }
         ?>
